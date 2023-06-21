03335d9ca214a63a2e5816c46994b3a5dc04fdd5
LimisiMike <michaellimisi7@gmail.com>
2023-06-21 12:39:58 +0300

    This was fun!

--- server/app.py
+++ server/app.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
+    
+# After creating our app and models, follow the steps below in the terminal;
+    # export FLASK_APP=app.py
+    #export FLASK_RUN_PORT=5555
+    #flask db init
+
+# In the models.py, under the app.config, Let's heed that warning and set SQLALCHEMY_TRACK_MODIFICATIONS to False. => app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# We can jump straight into migrating:
+     #   $flask db revision --autogenerate -m'Create tables owners, pets'
+     
+#...and pushing those migrations to our database:
+        # $flask db upgrade head
